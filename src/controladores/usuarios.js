6231ad528c1a472f5669d3afa3f0280418eae6eb
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-02-18 19:55:46 -0300

    feat: Adicionar função atualizarPerfil

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -51,11 +51,71 @@ const obterPerfil = async (req, res) => {
   return res.status(200).json(req.usuario);
 }
 
-// const atualizarPerfil = async (req, res) => {
+const atualizarPerfil = async (req, res) => {
+  const { nome, email, senha, nome_loja } = req.body;
 
-// }
+  if (!nome && !email && !senha && !nome_loja) {
+    return res.status(404).json("É obrigatório informar ao menos 1 campo para atualização!");
+  }
+
+  try {
+    const body = {};
+    const params = [];
+    let n = 1;
+
+    if (nome) {
+      body.nome = nome;
+      params.push(`nome = $${n}`);
+      n++;
+    }
+
+    if (email) {
+      if (email != req.usuario.email) {
+        const { rowCount: quantidadeUsuarios } = await conexao.query('select * from usuarios where email = $1', [email]);
+
+        if (quantidadeUsuarios > 0) {
+          return res.status(400).json("O email já existe!");
+        }
+      }
+
+      body.email = email;
+      params.push(`email = $${n}`);
+      n++;
+    }
+
+    if (senha) {
+      body.senha = await bcrypt.hash(senha, 10);
+      params.push(`senha = $${n}`);
+      n++;
+    }
+
+    if (nome_loja) {
+      body.nome_loja = nome_loja;
+      params.push(`nome_loja = $${n}`);
+      n++;
+    }
+
+    const valores = Object.values(body);
+
+    valores.push(req.usuario.id);
+
+    const query = `update usuarios set ${params.join(', ')} where id = $${n}`;
+
+    const usuarioAtualizado = await conexao.query(query, valores);
+
+    if (usuarioAtualizado.rowCount == 0) {
+      return res.status(400).json("O usuário não foi atualziado.");
+    }
+
+    return res.status(200).json("O usuário foi atualizado com sucesso!");
+
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+}
 
 module.exports = {
   cadastrarUsuario,
-  obterPerfil
+  obterPerfil,
+  atualizarPerfil
 }
