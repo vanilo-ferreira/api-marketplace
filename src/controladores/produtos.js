2557801719d54f52619a345852e09a8332cd7ef8
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-02-18 21:49:00 -0300

    feat: Adicionar rota listarProdutos

--- src/controladores/produtos.js
+++ src/controladores/produtos.js
@@ -1,11 +1,10 @@
 const conexao = require('../conexao');
-const bcrypt = require('bcrypt');
 
 const cadastrarProduto = async (req, res) => {
   const { usuario } = req;
-  const { nome, estoque, preco, categoria, descricao, imagem} = req.body;
+  const { nome, estoque, preco, categoria, descricao, imagem } = req.body;
 
-  if(!nome){
+  if (!nome) {
     return res.status(404).json('O campo nome é obrigatório');
   }
 
@@ -21,20 +20,43 @@ const cadastrarProduto = async (req, res) => {
     return res.status(404).json('O campo descricao é obrigatório');
   }
 
-  try{
+  try {
     const query = 'insert into produtos (usuario_id, nome, estoque, preco, categoria, descricao, imagem) values ($1, $2, $3, $4, $5, $6, $7)';
     const produtoCadastrado = await conexao.query(query, [usuario.id, nome, estoque, preco, categoria, descricao, imagem]);
 
-    if (produtoCadastrado.rowCount == 0){
+    if (produtoCadastrado.rowCount == 0) {
       return res.status(404).json('O produto não foi cadastrado!');
     }
 
     return res.status(201).json('O produto foi cadastrado com sucesso!');
-  }catch (error) {
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+}
+
+const listarProdutos = async (req, res) => {
+  const { usuario } = req;
+  const { categoria } = req.query;
+
+  try {
+    let condicao = '';
+    const params = [];
+
+    if (categoria) {
+      condicao += 'and categoria ilike $2';
+      params.push(`%${categoria}%`);
+    }
+
+    const query = `select * from produtos where usuario_id = $1 ${condicao}`;
+    const { rows: produtos } = await conexao.query(query, [usuario.id, ...params]);
+
+    return res.status(200).json(produtos);
+  } catch (error) {
     return res.status(400).json(error.message);
   }
 }
 
 module.exports = {
-  cadastrarProduto
+  cadastrarProduto,
+  listarProdutos
 }
